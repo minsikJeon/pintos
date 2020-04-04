251d05e3ed3aad5704858af310b6d3bb4c076100
minsikJeon <mike8664@kaist.ac.kr>
2020-04-04 11:13:54 +0900

    check

--- threads/synch.c
+++ threads/synch.c
@@ -110,7 +110,13 @@ sema_up (struct semaphore *sema) {
 
 
     /*priority preemption?*/
+
+   if(!intr_context()){
+      test_yield();
+   }
 	intr_set_level (old_level);
+
+   thread_yield();
 }
 
 static void sema_test_helper (void *sema_);
