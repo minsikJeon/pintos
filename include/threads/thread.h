6ba9cf77c4a5430b7e8344017b489ebac1bba7b0
minsikJeon <mike8664@kaist.ac.kr>
2020-04-05 23:26:51 +0900

    finalplz

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -24,11 +24,6 @@ typedef int tid_t;
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
 
-/*Thread nice values. */
-#define NICE_MAX 20
-#define NICE_DEFAULT 0
-#define NICE_MIN -20
-
 /* A kernel thread or user process.
  *
  * Each thread structure is stored in its own 4 kB page.  The
@@ -103,13 +98,6 @@ struct thread {
 	struct list locks;
     int init_priority;
 
-	/*--------------------My implementation----------------*/
-	int nice;
-	int recent_cpu;
-
-	struct list_elem allelem;
-	/*-----------------------------------------------------*/
-
 
 #ifdef USERPROG
 	/* Owned by userprog/process.c. */
@@ -169,10 +157,6 @@ bool thread_compare_priority(const struct list_elem *, const struct list_elem *,
 /*--------------------*/
 void priority_donation(struct thread *, int);
 
-/*myimplementation*/
-typedef void thread_action_func(struct thread *t, void *aux);
-void thread_foreach( thread_action_func *, void *);
-/*------------------*/
 
 void do_iret (struct intr_frame *tf);
 
